2efb18f2da45f2fc90e4128c4c266dc4de89669f
Tomáš Milet <imilet@fit.vutbr.cz>
2026-04-14 17:35:04 +0200

    context

--- main.cpp
+++ main.cpp
@@ -1,6 +1,25 @@
 #include<iostream>
 
+#include<SDL3/SDL.h>
+
 int main(int argc,char*argv[]){
   std::cerr << "PAPOUCH ŽERE OŘECHY." << std::endl;
+  auto window = SDL_CreateWindow("IZG2026",1024,768,SDL_WINDOW_OPENGL);
+
+  auto context = SDL_GL_CreateContext(window);
+
+  bool running = true;
+  while(running){//main loop
+    SDL_Event event;
+    while(SDL_PollEvent(&event)){//event loop
+      if(event.type == SDL_EVENT_QUIT)running = false;
+
+    }
+
+    SDL_GL_SwapWindow(window);
+  }
+
+  SDL_GL_DestroyContext(context);
+  SDL_DestroyWindow(window);
   return 0;
 }
