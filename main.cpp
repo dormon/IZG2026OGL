8933bbea27eba1e1adc64c4572dcd3f04cf2a670
Tomáš Milet <imilet@fit.vutbr.cz>
2026-04-14 17:52:31 +0200

    triangle

--- main.cpp
+++ main.cpp
@@ -6,6 +6,26 @@
 
 using namespace ge::gl;
 
+GLuint createShader(GLenum type,std::string const&src){
+  auto s = glCreateShader(type);
+  char const*sss[]={
+    src.c_str()
+  };
+  glShaderSource(s,1,sss,0);
+  glCompileShader(s);
+  return s;
+}
+
+GLuint createProgram(std::vector<GLuint>ss){
+  auto s = glCreateProgram();
+
+  for(auto const&sh:ss)
+    glAttachShader(s,sh);
+
+  glLinkProgram(s);
+  return s;
+}
+
 int main(int argc,char*argv[]){
   std::cerr << "PAPOUCH ŽERE OŘECHY." << std::endl;
   auto window = SDL_CreateWindow("IZG2026",1024,768,SDL_WINDOW_OPENGL);
@@ -13,6 +33,26 @@ int main(int argc,char*argv[]){
   auto context = SDL_GL_CreateContext(window);
 
   ge::gl::init();
+
+  auto vsSrc = R".(
+  #version 460
+  void main(){
+    gl_Position = vec4(gl_VertexID%2,gl_VertexID/2,0,1);
+  }
+  ).";
+
+  auto fsSrc = R".(
+  #version 460
+  out vec4 fColor;
+  void main(){
+    fColor = vec4(0,1,0,1);    
+  }
+  ).";
+
+  auto vs = createShader(GL_VERTEX_SHADER,vsSrc);
+  auto fs = createShader(GL_FRAGMENT_SHADER,fsSrc);
+
+  auto prg = createProgram({vs,fs});
 
   bool running = true;
   while(running){//main loop
@@ -25,10 +65,13 @@ int main(int argc,char*argv[]){
 
 
     glPointSize(10);
+
     glClearColor(1,0,0,1);
     glClear(GL_COLOR_BUFFER_BIT);
 
-    glDrawArrays(GL_POINTS,0,1);
+
+    glUseProgram(prg);
+    glDrawArrays(GL_TRIANGLES,0,3);
 
     SDL_GL_SwapWindow(window);
   }
